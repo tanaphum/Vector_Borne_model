5d0d2675fce9044e6e538c2c8b743482916e9f65
Tanaphum Wichaita <tana_jumbo@hotmail.com>
2024-11-26 10:13:32 +0700

    update
    - add birth rate and death rate
    - change color lines
    - change time range

--- server.R
+++ server.R
@@ -10,12 +10,14 @@ function(input, output, session) {
   values <- reactiveValues()
   
   # SIR model parameters update
-  observeEvent(c(input$bites_rate, input$T_HM, input$T_MH, input$infectious_period, input$mu_M), {
+  observeEvent(c(input$bites_rate, input$T_HM, input$T_MH, input$infectious_period,
+                 input$birth_M,input$death_M), {
     values$bites_rate <- input$bites_rate
     values$T_HM <- input$T_HM
     values$T_MH <- input$T_MH
     values$infectious_period <- input$infectious_period
-    values$mu_M <- input$mu_M
+    values$birth_M <- input$birth_M
+    values$death_M <- input$death_M
   })
   
   # Define the SIR model function
@@ -30,8 +32,8 @@ function(input, output, session) {
       Susceptible_human_change <- -force_of_infection_on_human * Susceptible_human
       Infectious_human_change <- force_of_infection_on_human * Susceptible_human - Infectious_human / infectious_period
       Recovered_human_change <- Infectious_human / infectious_period
-      Susceptible_mosquito_change <- mu_M * Total_mosquito_population - (force_of_infection_on_mosquito + mu_M) * Susceptible_mosquito
-      Infectious_mosquito_change <- force_of_infection_on_mosquito * Susceptible_mosquito - mu_M * Infectious_mosquito
+      Susceptible_mosquito_change <- birth_M * Total_mosquito_population - (force_of_infection_on_mosquito + death_M) * Susceptible_mosquito
+      Infectious_mosquito_change <- force_of_infection_on_mosquito * Susceptible_mosquito - death_M * Infectious_mosquito
       
       return(list(c(
         Susceptible_human_change, Infectious_human_change, Recovered_human_change,
@@ -41,13 +43,17 @@ function(input, output, session) {
   }
   
   output$humanPlot <- renderPlotly({
+
     parameters <- c(
       bites = values$bites_rate,
       T_HM = values$T_HM,
       T_MH = values$T_MH,
       infectious_period = values$infectious_period,
-      mu_M = values$mu_M
+      birth_M = values$birth_M,
+      death_M = values$death_M
     )
+    
+    
     
     state <- c(
       Susceptible_human = 149998,
@@ -57,7 +63,7 @@ function(input, output, session) {
       Infectious_mosquito = 10
     )
     
-    times <- seq(0, 1500, by = 1)
+    times <- seq(0, 1000, by = 1)
     output <- ode(y = state, times = times, func = dengue_base, parms = parameters)
     output_df <- as.data.frame(output)
     
@@ -65,7 +71,9 @@ function(input, output, session) {
       select(time, Susceptible_human, Infectious_human, Recovered_human) %>%
       pivot_longer(cols = -time, names_to = "Compartment", values_to = "Count")
     
-    plot_ly(human_df, x = ~time, y = ~Count, color = ~Compartment, type = 'scatter', mode = 'lines') %>%
+    color_map <- c(Susceptible_human = "blue3", Infectious_human = "red2", Recovered_human = "green2")
+    
+    plot_ly(human_df, x = ~time, y = ~Count, color = ~Compartment, colors = color_map, type = 'scatter', mode = 'lines') %>%
       layout(
         title = "SIR Human Population Dynamics",
         xaxis = list(title = "Time"),
@@ -79,7 +87,8 @@ function(input, output, session) {
       T_HM = values$T_HM,
       T_MH = values$T_MH,
       infectious_period = values$infectious_period,
-      mu_M = values$mu_M
+      birth_M = values$birth_M,
+      death_M = values$death_M
     )
     
     state <- c(
@@ -90,7 +99,7 @@ function(input, output, session) {
       Infectious_mosquito = 10
     )
     
-    times <- seq(0, 1500, by = 1)
+    times <- seq(0, 1000, by = 1)
     output <- ode(y = state, times = times, func = dengue_base, parms = parameters)
     output_df <- as.data.frame(output)
     
@@ -98,7 +107,10 @@ function(input, output, session) {
       select(time, Susceptible_mosquito, Infectious_mosquito) %>%
       pivot_longer(cols = -time, names_to = "Compartment", values_to = "Count")
     
-    plot_ly(mosquito_df, x = ~time, y = ~Count, color = ~Compartment, type = 'scatter', mode = 'lines') %>%
+    color_map <- c(Susceptible_mosquito = "blue3", Infectious_mosquito = "red2")
+    
+    
+    plot_ly(mosquito_df, x = ~time, y = ~Count, color = ~Compartment, colors = color_map, type = 'scatter', mode = 'lines') %>%
       layout(
         title = "SIR Mosquito Population Dynamics",
         xaxis = list(title = "Time"),
@@ -108,12 +120,13 @@ function(input, output, session) {
   
   # SEIR model parameters update
   observeEvent(c(input$bites_rate_E, input$T_HM_E, input$T_MH_E, input$infectious_period_E, 
-                 input$mu_M_E, input$exposed_period_H, input$exposed_period_M), {
+                 input$birth_M_E,input$death_M_E, input$exposed_period_H, input$exposed_period_M), {
                    values$bites_rate_E <- input$bites_rate_E
                    values$T_HM_E <- input$T_HM_E
                    values$T_MH_E <- input$T_MH_E
                    values$infectious_period_E <- input$infectious_period_E
-                   values$mu_M_E <- input$mu_M_E
+                   values$birth_M_E <- input$birth_M_E
+                   values$death_M_E <- input$death_M_E
                    values$exposed_period_H <- input$exposed_period_H
                    values$exposed_period_M <- input$exposed_period_M
                  })
@@ -131,9 +144,9 @@ function(input, output, session) {
       Exposed_human_change <- force_of_infection_on_human * Susceptible_human - Exposed_human / exposed_period_H
       Infectious_human_change <- Exposed_human / exposed_period_H - Infectious_human / infectious_period
       Recovered_human_change <- Infectious_human / infectious_period
-      Susceptible_mosquito_change <- mu_M * Total_mosquito_population - (force_of_infection_on_mosquito + mu_M) * Susceptible_mosquito
-      Exposed_mosquito_change <- force_of_infection_on_mosquito * Susceptible_mosquito - Exposed_mosquito / exposed_period_M
-      Infectious_mosquito_change <- Exposed_mosquito / exposed_period_M - mu_M * Infectious_mosquito
+      Susceptible_mosquito_change <- birth_M_E * Total_mosquito_population - (force_of_infection_on_mosquito + death_M_E) * Susceptible_mosquito
+      Exposed_mosquito_change <- force_of_infection_on_mosquito * Susceptible_mosquito - Exposed_mosquito * (1 / exposed_period_M + death_M_E)
+      Infectious_mosquito_change <- Exposed_mosquito * (1 / exposed_period_M) - death_M_E * Infectious_mosquito
       
       return(list(c(
         Susceptible_human_change, Exposed_human_change, Infectious_human_change, Recovered_human_change,
@@ -148,7 +161,8 @@ function(input, output, session) {
       T_HM = values$T_HM_E,
       T_MH = values$T_MH_E,
       infectious_period = values$infectious_period_E,
-      mu_M = values$mu_M_E,
+      birth_M_E = values$birth_M_E,
+      death_M_E = values$death_M_E,
       exposed_period_H = values$exposed_period_H,
       exposed_period_M = values$exposed_period_M
     )
@@ -163,7 +177,7 @@ function(input, output, session) {
       Infectious_mosquito = 10
     )
     
-    times <- seq(0, 1500, by = 1)
+    times <- seq(0, 1000, by = 1)
     output <- ode(y = state, times = times, func = dengue_with_E, parms = parameters)
     output_df <- as.data.frame(output)
     
@@ -171,7 +185,9 @@ function(input, output, session) {
       select(time, Susceptible_human, Exposed_human, Infectious_human, Recovered_human) %>%
       pivot_longer(cols = -time, names_to = "Compartment", values_to = "Count")
     
-    plot_ly(human_df, x = ~time, y = ~Count, color = ~Compartment, type = 'scatter', mode = 'lines') %>%
+    color_map <- c(Susceptible_human = "blue3",Exposed_human = "yellow2", Infectious_human = "red2", Recovered_human = "green2")
+    
+    plot_ly(human_df, x = ~time, y = ~Count, color = ~Compartment, colors = color_map, type = 'scatter', mode = 'lines') %>%
       layout(
         title = "SEIR Human Population Dynamics",
         xaxis = list(title = "Time"),
@@ -185,7 +201,8 @@ function(input, output, session) {
       T_HM = values$T_HM_E,
       T_MH = values$T_MH_E,
       infectious_period = values$infectious_period_E,
-      mu_M = values$mu_M_E,
+      birth_M_E = values$birth_M_E,
+      death_M_E = values$death_M_E,
       exposed_period_H = values$exposed_period_H,
       exposed_period_M = values$exposed_period_M
     )
@@ -200,7 +217,7 @@ function(input, output, session) {
       Infectious_mosquito = 10
     )
     
-    times <- seq(0, 1500, by = 1)
+    times <- seq(0, 1000, by = 1)
     output <- ode(y = state, times = times, func = dengue_with_E, parms = parameters)
     output_df <- as.data.frame(output)
     
@@ -208,7 +225,9 @@ function(input, output, session) {
       select(time, Susceptible_mosquito, Exposed_mosquito, Infectious_mosquito) %>%
       pivot_longer(cols = -time, names_to = "Compartment", values_to = "Count")
     
-    plot_ly(mosquito_df, x = ~time, y = ~Count, color = ~Compartment, type = 'scatter', mode = 'lines') %>%
+    color_map <- c(Susceptible_mosquito = "blue3",Exposed_mosquito = "yellow2", Infectious_mosquito = "red2")
+    
+    plot_ly(mosquito_df, x = ~time, y = ~Count, color = ~Compartment,colors=color_map, type = 'scatter', mode = 'lines') %>%
       layout(
         title = "SEIR Mosquito Population Dynamics",
         xaxis = list(title = "Time"),
